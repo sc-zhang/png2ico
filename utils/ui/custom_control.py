0360dc9a2e8f908caece9b14b8cbc11ecee11645
sc-zhang <zsc-zhang@foxmail.com>
2024-03-25 16:37:02 +0800

    fix bug

--- utils/ui/custom_control.py
+++ utils/ui/custom_control.py
@@ -19,22 +19,23 @@ class DragLabel(QLabel):
     def dropEvent(self, event):
         if event.mimeData().hasUrls():
             img_path = event.mimeData().urls()[0].toLocalFile()
-            if not img_path.endswith('.png') or not img_path.endswith(".ico"):
-                event.ignore()
-            try:
-                img = Image.open(img_path)
-                self.setPixmap(QPixmap.fromImage(ImageQt.ImageQt(img)).scaled(128, 128))
-                if img_path.endswith(".png"):
-                    icon_sizes = [(16, 16), (32, 32), (64, 64), (128, 128), (256, 256)]
-                    icon_path = img_path[:-4]+".ico"
-                    img.save(icon_path, format="ico", sizes=icon_sizes)
-                    QMessageBox.information(self, "PNG2ICO", "Image saved: {}".format(icon_path))
-                else:
-                    png_path = img_path[:-4] + ".png"
-                    img.save(png_path, format="png", size=(256, 256))
-                    QMessageBox.information(self, "PNG2ICO", "Image saved: {}".format(png_path))
-            except Exception:
-                QMessageBox.critical(self, "Error", format_exc())
+            if img_path.endswith('.png') or img_path.endswith(".ico"):
+                try:
+                    img = Image.open(img_path)
+                    self.setPixmap(QPixmap.fromImage(ImageQt.ImageQt(img)).scaled(128, 128))
+                    if img_path.endswith(".png"):
+                        icon_sizes = [(16, 16), (32, 32), (64, 64), (128, 128), (256, 256)]
+                        icon_path = img_path[:-4]+".ico"
+                        img.save(icon_path, format="ico", sizes=icon_sizes)
+                        QMessageBox.information(self, "PNG2ICO", "Image saved: {}".format(icon_path))
+                    else:
+                        png_path = img_path[:-4] + ".png"
+                        img.save(png_path, format="png", size=(256, 256))
+                        QMessageBox.information(self, "PNG2ICO", "Image saved: {}".format(png_path))
+                except Exception:
+                    QMessageBox.critical(self, "Error", format_exc())
+            else:
+                QMessageBox.information(self, "PNG2ICO", "File format not supported")
             event.accept()
         else:
             event.ignore()
