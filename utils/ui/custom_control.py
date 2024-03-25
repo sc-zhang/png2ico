8f77935d20fe1035a825b9b8facf4eb3b7d7bc33
sc-zhang <zsc-zhang@foxmail.com>
2024-03-25 16:46:29 +0800

    format file

--- utils/ui/custom_control.py
+++ utils/ui/custom_control.py
@@ -3,6 +3,7 @@ from PySide6.QtWidgets import QLabel, QMessageBox
 from PIL import Image, ImageQt
 from traceback import format_exc
 
+
 class DragLabel(QLabel):
 
     def __init__(self, parent=None):
@@ -25,7 +26,7 @@ class DragLabel(QLabel):
                     self.setPixmap(QPixmap.fromImage(ImageQt.ImageQt(img)).scaled(128, 128))
                     if img_path.endswith(".png"):
                         icon_sizes = [(16, 16), (32, 32), (64, 64), (128, 128), (256, 256)]
-                        icon_path = img_path[:-4]+".ico"
+                        icon_path = img_path[:-4] + ".ico"
                         img.save(icon_path, format="ico", sizes=icon_sizes)
                         QMessageBox.information(self, "PNG2ICO", "Image saved: {}".format(icon_path))
                     else:
